5a2dc082c7563d2888da5eacbbc5c11f01952b49
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-25 14:54:22 +0800

    Implement clearing screen when operand input

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-let operand1;
-let operand2;
-let operator;
-
 const operate = (operator, operand1, operand2) => {
   switch(operator) {
     case "+":
@@ -22,12 +18,31 @@ const operate = (operator, operand1, operand2) => {
   }
 }
 
+let operand1;
+let operand2;
+let operator;
+let newNumber = true;
+
 const buttons = document.querySelector('.buttons');
 const display = document.querySelector('#display');
 buttons.addEventListener('click', (event) => {
   const target = event.target;
   if(target.children.length === 0) {
-    display.textContent += target.textContent;
+    if(target.id === "op_cl") {
+      display.textContent = "";
+      operand1 = null;
+      operand2 = null;
+      operator = null;
+    } else if(target.id.includes('op')) {
+      newNumber = true;
+    } else { 
+      if(newNumber) {
+        display.textContent = target.textContent;
+        newNumber = false;
+      } else { 
+        display.textContent += target.textContent;
+      }
+    }
   } 
-})
+});
 
