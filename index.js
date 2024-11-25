3683d289b2f9eb0575ac37b0c8000740f44a6a51
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-25 11:54:28 +0800

    Defined function for handling calculator operations

---
+++ index.js
@@ -0,0 +1,19 @@
+const operations = require('./scripts/operations');
+let operand1;
+let operand2;
+let operator;
+
+const operate = (operator, operand1, operand2) => {
+  switch(operator) {
+    case "+":
+      return operations.add(operand1, operand2);     
+    case "-": 
+      return operations.subtract(operand1, operand2);     
+    case "X":
+      return operations.multiply(operand1, operand2);     
+    case "÷":
+      return operations.divide(operand1, operand2);     
+    default:
+      return "ERR";
+  }
+}
