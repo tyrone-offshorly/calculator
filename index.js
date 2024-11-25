81881aa81800cf74706aef9b6bfd028b2465b493
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-25 13:37:51 +0800

    Display button presses on the screen

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const operations = require('./scripts/operations');
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
 
 let operand1;
 let operand2;
@@ -7,15 +10,24 @@ let operator;
 const operate = (operator, operand1, operand2) => {
   switch(operator) {
     case "+":
-      return operations.add(operand1, operand2);     
+      return add(operand1, operand2);     
     case "-": 
-      return operations.subtract(operand1, operand2);     
+      return subtract(operand1, operand2);     
     case "X":
-      return operations.multiply(operand1, operand2);     
+      return multiply(operand1, operand2);     
     case "÷":
-      return operations.divide(operand1, operand2);     
+      return divide(operand1, operand2);     
     default:
       return "ERR";
   }
 }
+
+const buttons = document.querySelector('.buttons');
+const display = document.querySelector('#display');
+buttons.addEventListener('click', (event) => {
+  const target = event.target;
+  if(target.children.length === 0) {
+    display.textContent += target.textContent;
+  } 
+})
 
